f236b46e7bf2c91914f4b0766ebc94d5a4301e22
Livingstone Del Monte <livi.monte@gmail.com>
2020-03-29 18:23:07 -0300

    feat: web version with react and axios

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -16,6 +16,6 @@ routes.delete('/incidents/:id', IncidentController.delete);
 
 routes.get('/profile', ProfileController.index);
 
-routes.get('/sessions', SessionController.create);
+routes.post('/sessions', SessionController.create);
 
 module.exports = routes;
